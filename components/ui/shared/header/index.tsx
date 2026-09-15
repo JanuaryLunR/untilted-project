0b17f2751a7c1fe369e9fc08e0ca49e4bd7e742a
Roman <rolu199947@gmail.com>
2026-09-15 20:01:53 +0300

    Add sheet menu

--- components/ui/shared/header/index.tsx
+++ components/ui/shared/header/index.tsx
@@ -1,10 +1,7 @@
-import { ShoppingCart, UserIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import { Button } from "../../button";
 import { APP_NAME } from "@/lib/constants";
-import ModeToggle from "./mode-toggle";
-
+import Menu from "./menu";
 
 const Header = () => {
   return (
@@ -19,22 +16,12 @@ const Header = () => {
               width={48}
               priority={true}
             />
-            <span className="hidden lg:block font-bold text-2xl ml-3">{APP_NAME}</span>
+            <span className="hidden lg:block font-bold text-2xl ml-3">
+              {APP_NAME}
+            </span>
           </Link>
         </div>
-        <div className="space-x-2">
-            <ModeToggle/>
-            <Button asChild variant='ghost'>
-                <Link href='/cart'> 
-                <ShoppingCart /> Cart
-                </Link>
-            </Button>
-            <Button asChild>
-                <Link href='/sign-in'> 
-                <UserIcon /> Sign In
-                </Link>
-            </Button>
-        </div>
+        <Menu />
       </div>
     </header>
   );
