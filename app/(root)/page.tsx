b1ca1b0410f24ded883b2372abb71f92f14aa241
Roman <rolu199947@gmail.com>
2026-09-16 22:00:45 +0300

    Add Product card component and Product list

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,16 @@
+import ProductList from "@/components/ui/shared/product/product-list";
+import sampleData from "@/db/sample-data";
+
 const Homepage = () => {
-  return <>Untilted-project-app</>;
+  return (
+    <>
+      <ProductList
+        data={sampleData.products}
+        title="Newest Arrivals"
+        limit={4}
+      />
+    </>
+  );
 };
 
 export default Homepage;
